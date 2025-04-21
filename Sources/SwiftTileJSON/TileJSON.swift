bad28f54eb95009dc272bfc2df808c7bc667e723
Scott Hoyt <scottrhoyt@gmail.com>
2025-04-20 20:28:51 -0700

    Extract VectorLayer to separate file.

--- Sources/SwiftTileJSON/TileJSON.swift
+++ Sources/SwiftTileJSON/TileJSON.swift
@@ -71,25 +71,6 @@ struct TileJSON: Codable {
         case tms = "tms"
     }
     
-    /// Describes one layer of vector tile data
-    struct VectorLayer: Codable {
-        /// REQUIRED. A string value representing the layer id.
-        let id: String
-        
-        /// REQUIRED. An object whose keys and values are the names and descriptions of attributes.
-        /// If no fields are present, it should be an empty object.
-        let fields: [String: String]
-        
-        /// OPTIONAL. A human-readable description of the layer.
-        let description: String?
-        
-        /// OPTIONAL. The lowest zoom level whose tiles this layer appears in.
-        let minzoom: Int?
-        
-        /// OPTIONAL. The highest zoom level whose tiles this layer appears in.
-        let maxzoom: Int?
-    }
-    
     // MARK: - CodingKeys
     
     private enum CodingKeys: String, CodingKey {
