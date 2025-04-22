c7590acaca1fe72735a18612e1347f79a9c61669
Scott Hoyt <scottrhoyt@gmail.com>
2025-04-21 17:47:30 -0700

    Move zoom level validation to Valid.

--- Sources/SwiftTileJSON/TileJSON.swift
+++ Sources/SwiftTileJSON/TileJSON.swift
@@ -132,6 +132,26 @@ extension TileJSON {
         static let zoomRange: ClosedRange<Int> = 0...30
         static let latitudeRange: ClosedRange<Double> = -90...90
         static let longitudeRange: ClosedRange<Double> = -180...180
+        
+        /// Validates that zoom levels are between 0...30 and maxZoom >= minzoom.
+        static func zoomLevels(minZoom: Int?, maxZoom: Int?) -> (minZoom: Int?, maxZoom: Int?) {
+            var validatedMinZoom: Int?
+            var validatedMaxZoom: Int?
+            
+            if let minZoom = minZoom {
+                validatedMinZoom = Valid.zoomRange.contains(minZoom) ? minZoom : nil
+            }
+            
+            if let maxZoom = maxZoom {
+                validatedMaxZoom = Valid.zoomRange.contains(maxZoom) ? maxZoom : nil
+            }
+            
+            if let validatedMinZoom = validatedMinZoom, let validatedMaxZoom = validatedMaxZoom, validatedMinZoom > validatedMaxZoom {
+                return (nil, nil)
+            }
+            
+            return (validatedMinZoom, validatedMaxZoom)
+        }
     }
     
     /// Custom initializer to validate TileJSON data during decoding
@@ -192,7 +212,7 @@ extension TileJSON {
         }
         
         // Decove OPTIONAL zoom levels. `nil` if invalid.
-        (minZoom, maxZoom) = TileJSON.validate(
+        (minZoom, maxZoom) = Valid.zoomLevels(
             minZoom: try? container.decodeIfPresent(Int.self, forKey: .minZoom),
             maxZoom: try? container.decodeIfPresent(Int.self, forKey: .maxZoom)
         )
@@ -268,25 +288,5 @@ extension TileJSON {
         }
         
         return false
-    }
-    
-    /// Validates that zoom levels are between 0...30 and maxZoom >= minzoom.
-    private static func validate(minZoom: Int?, maxZoom: Int?) -> (minZoom: Int?, maxZoom: Int?) {
-        var validatedMinZoom: Int?
-        var validatedMaxZoom: Int?
-        
-        if let minZoom = minZoom {
-            validatedMinZoom = Valid.zoomRange.contains(minZoom) ? minZoom : nil
-        }
-        
-        if let maxZoom = maxZoom {
-            validatedMaxZoom = Valid.zoomRange.contains(maxZoom) ? maxZoom : nil
-        }
-        
-        if let validatedMinZoom = validatedMinZoom, let validatedMaxZoom = validatedMaxZoom, validatedMinZoom > validatedMaxZoom {
-            return (nil, nil)
-        }
-        
-        return (validatedMinZoom, validatedMaxZoom)
     }
 }
